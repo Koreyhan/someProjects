e7a140fc1f29e6f0edfc8cdb21dd896fb4dd371c
Mac blog <746326947@qq.com>
2016-09-13 21:10:51 +0800

    change 008

--- 008.game2048/js/game2048.js
+++ 008.game2048/js/game2048.js
@@ -29,6 +29,15 @@ $(function(){
 		}
 		
 	});
+
+	$("body").swipe( {
+        //Generic swipe handler for all directions
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+          console.log("You swiped " + direction );  
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+         threshold:0
+	});
 });
 
 
